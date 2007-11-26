06ba682fd399aa65576815ff42a4f04757dc6497
serkankaba <serkankaba@14afa583-5d3d-0410-b483-632d3007c162>
2007-11-26 21:47:04 +0000

    0.1 sürümü

---
+++ tags/0.1/src/com/googlecode/jkota/BaseADSLKota.java
@@ -0,0 +1,64 @@
+package com.googlecode.jkota;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Timer;
+
+import net.sourceforge.blowfishj.BlowfishInputStream;
+import net.sourceforge.blowfishj.BlowfishOutputStream;
+
+public abstract class BaseADSLKota {
+	
+	public abstract void updateKota(String kota);
+	public abstract void firstTime();
+	private Properties settings;
+	private byte masterKey[];
+	
+	public BaseADSLKota() {
+		masterKey=new byte[0];
+		settings=new Properties();
+	}
+	
+	public final void setMasterKey(String masterKey) {
+		this.masterKey = masterKey.getBytes(Charset.forName("UTF-8"));
+	}
+
+	public final void startTimer() {
+		Timer t = new Timer();
+		t.schedule(new Updater(this),new Date(), 60000*getIntSetting("updateinterval",10));
+	}
+	
+	public void storeSettings() throws IOException {
+		BlowfishOutputStream out=new BlowfishOutputStream(
+				masterKey,0,masterKey.length,
+				new FileOutputStream(System.getProperty("user.home") + File.separator+".jkota")
+		);
+		settings.store(out,"");
+		out.flush();
+		out.close();
+	}
+	
+	public void readSettings() throws IOException {
+		File settingsFile=new File(System.getProperty("user.home") + File.separator+".jkota");
+		if(!settingsFile.exists())
+			firstTime();
+		else
+			settings.load(new BlowfishInputStream(masterKey,0,masterKey.length,new FileInputStream(settingsFile)));
+	}
+	
+	public void setSetting(String key,String value) { settings.setProperty(key, value); }
+	public String getSetting(String key) { return settings.getProperty(key); }
+	public void setIntSetting(String key,int value) { settings.setProperty(key, Integer.toString(value)); }
+	public int getIntSetting(String key,int defaultSetting) {
+		String valueString=settings.getProperty(key);
+		if(valueString==null)
+			return defaultSetting;
+		else
+			return Integer.parseInt(valueString); 
+	}
+}
---
+++ tags/0.1/src/com/googlecode/jkota/swing/SwingSettings.java
@@ -0,0 +1,82 @@
+package com.googlecode.jkota.swing;
+
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+
+import com.googlecode.jkota.BaseADSLKota;
+
+public class SwingSettings extends JDialog implements ActionListener {
+	
+	private BaseADSLKota adslKota;
+	private JHelpText userName,password,apiKey;
+	private JSpinner updateInterval;
+	
+	public SwingSettings(BaseADSLKota adslKota) {
+		super((Frame)null,"Yapılandırma",true);
+		this.adslKota=adslKota;
+		setLayout(new BorderLayout());
+		JPanel settingsPanel=new JPanel(new GridLayout(0,2));
+		settingsPanel.add(new JLabel("Kullanıcı adı:"));
+		userName=new JHelpText(adslKota.getSetting("username"),"http://code.google.com/p/jkota/wiki/KullanimDokumani",false);
+		settingsPanel.add(userName);
+		settingsPanel.add(new JLabel("Şifre:"));
+		password=new JHelpText(adslKota.getSetting("password"),"http://code.google.com/p/jkota/wiki/KullanimDokumani",true);
+		settingsPanel.add(password);
+		settingsPanel.add(new JLabel("API Key:"));
+		apiKey=new JHelpText(adslKota.getSetting("apikey"),"http://code.google.com/p/jkota/wiki/KullanimDokumani",false);
+		settingsPanel.add(apiKey);
+		settingsPanel.add(new JLabel("Günceleme sıklığı:"));
+		updateInterval=new JSpinner(new SpinnerNumberModel(adslKota.getIntSetting("updateinterval",10),10,120,10));
+		JPanel updateIntervalPanel=new JPanel(new FlowLayout());
+		updateIntervalPanel.add(updateInterval);
+		updateIntervalPanel.add(new JLabel("dk."));
+		settingsPanel.add(updateIntervalPanel);
+		getContentPane().add(settingsPanel,BorderLayout.CENTER);
+		JButton ok=new JButton("Tamam");
+		ok.addActionListener(this);
+		getContentPane().add(ok,BorderLayout.SOUTH);
+		setSize(300,200);
+		setResizable(false);
+		SwingUtil.center(this);
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public void windowActivated(WindowEvent e) {}
+	public void windowClosed(WindowEvent e) {}
+	public void windowDeactivated(WindowEvent e) {}
+	public void windowDeiconified(WindowEvent e) {}
+	public void windowIconified(WindowEvent e) {}
+	public void windowOpened(WindowEvent e) {}
+	
+	public void windowClosing(WindowEvent e) {
+		
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		adslKota.setSetting("username", userName.getText());
+		adslKota.setSetting("password", password.getText());
+		adslKota.setSetting("apikey", apiKey.getText());
+		adslKota.setIntSetting("updateinterval", (Integer)updateInterval.getModel().getValue());
+		try {
+			adslKota.storeSettings();
+		} catch (IOException e1) {
+			SwingUtil.error(this, e1,"Ayarlar kaydedilirken hata");
+		}
+		dispose();
+	}	
+}
