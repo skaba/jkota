5f0f2d29e7961575e53a9933e393457eb5214d66
serkankaba <serkankaba@14afa583-5d3d-0410-b483-632d3007c162>
2008-07-26 08:31:06 +0000

    jkota-0.4 etiketlendi.

---
+++ tags/0.4/src/com/googlecode/jkota/BaseDownloader.java
@@ -0,0 +1,192 @@
+package com.googlecode.jkota;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+
+import org.xml.sax.SAXException;
+
+import com.googlecode.jkota.downloader.KabloNetDownloader;
+import com.googlecode.jkota.downloader.TTNetADSLDownloader;
+import com.meterware.httpunit.HttpUnitOptions;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebForm;
+import com.meterware.httpunit.WebResponse;
+
+public abstract class BaseDownloader {
+	protected String lastQuota="";
+	protected QuotaInfo[] quotas;
+	protected WebConversation conversation;
+	private Unit viewUnit;
+	public abstract boolean login(String username,String password);
+	public abstract boolean downloadQuota();
+
+	protected BaseDownloader() {
+		quotas=new QuotaInfo[0];
+		conversation=new WebConversation();
+		conversation.setHeaderField("User-agent","Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)");
+		HttpUnitOptions.setScriptingEnabled(false);//Disable Javascript parsing
+	}
+
+	public String getLastQuota() { return lastQuota; }
+	public int getQuotaSize() { return quotas.length; }
+	public QuotaInfo getQuota(int i) { return quotas[i]; }
+	public Unit getViewUnit() { return viewUnit; }
+
+	private static BaseDownloader theInstance;
+	private static String currentInstanceName;
+
+	public static synchronized BaseDownloader getInstance(String name) {
+		if(theInstance==null || !currentInstanceName.equals(name)) {
+			if("TTNet ADSL".equals(name))
+				theInstance= new TTNetADSLDownloader();
+			else if("Kablo Net".equals(name))
+				theInstance= new KabloNetDownloader();
+		}
+		currentInstanceName=name;
+		return theInstance;
+	}
+
+	public synchronized boolean update() {
+		SettingsManager settings=SettingsManager.getInstance();
+		LogManager logger=LogManager.getInstance();
+		logger.info("Güncelleme işlemine başlandı",true);
+		if (!login(settings.getSetting("username"),settings.getSetting("password"))) {
+			logger.warning("Güncelleme: Başarısız (Login işleminde hata)",true);
+			return false;
+		}
+		if(!downloadQuota()) {
+			logger.warning("Güncelleme: Başarısız (Kota bilgileri alımında hata)",true);
+			return false;
+		}
+		viewUnit=calculateUnit();
+		logger.info("Güncelleme: Başarılı ("+ lastQuota+")",true);
+		return true;
+	}
+
+	public static String[] getDownloaders() {
+		return new String[] {"TTNet ADSL","Kablo Net"};
+	}
+
+	private Unit calculateUnit() {
+		long max=0L;
+		for(int i=0;i<quotas.length;i++) {
+			QuotaInfo info = quotas[i];
+			if(info.getDownloadedBytes()>max)
+				max=info.getDownloadedBytes();
+			if(info.getUploadedBytes()>max)
+				max=info.getUploadedBytes();
+		}
+		Unit units[]=Unit.values();
+		for(int i=units.length-1;i>=0;i--) {
+			if(max>units[i].getDivider())
+				return units[i];
+		}
+		return Unit.BYTE;
+	}
+
+	private boolean downloadCaptcha(String url) {
+		LogManager logger=LogManager.getInstance();
+		try {
+			WebResponse response = conversation.getResponse(url);
+			copyStream(response.getInputStream(), new FileOutputStream(System.getProperty("java.io.tmpdir")+"/captcha"));
+			logger.info("Captcha alındı");
+			return true;
+		} catch (MalformedURLException e) {
+			logger.warning("Güvenlik kodu indirilirken hata",e);
+		} catch (IOException e) {
+			logger.warning("Güvenlik kodu indirilirken hata",e);
+		} catch (SAXException e) {
+			logger.warning("Güvenlik kodu indirilirken hata",e);
+		}
+		return false;
+	}
+
+	protected String extractCaptcha(String url) {
+		if (downloadCaptcha(url)) {
+			LogManager logger=LogManager.getInstance();
+			try {
+				SettingsManager settings=SettingsManager.getInstance();
+				WebResponse response=conversation.getResponse("http://jkota.googlecode.com/svn/trunk/uploadform.html");
+				WebForm upload_captcha=response.getForms()[0];
+				upload_captcha.setParameter("api_key", settings.getSetting("apikey"));
+				upload_captcha.setParameter("captcha_url",url);
+				upload_captcha.setParameter("file", new File(System.getProperty("java.io.tmpdir")+"/captcha"));
+				String guid="";
+				while(guid.equals("")) {
+					response=upload_captcha.submit();
+					String responseText=response.getText();
+					if(responseText.startsWith("SUCCESS: captcha_id="))
+						guid=responseText.substring(20);
+					else {
+						logger.warning
+						(
+								"Güvenlik kodu gönderilirken hata: "+
+								responseText.substring(9)
+						);
+						return "";
+					}
+				}
+				logger.info("Captcha gönderildi");
+				guid=guid.substring(0,36);
+				response=conversation.getResponse("http://jkota.googlecode.com/svn/trunk/resultform.html");
+				WebForm get_result=response.getForms()[0];
+				get_result.setParameter("api_key", settings.getSetting("apikey"));
+				get_result.setParameter("captcha_id", guid);
+				String captcha="";
+				while(captcha.equals("")) {
+					response=get_result.submit();
+					String responseText=response.getText();
+					if(responseText.startsWith("SUCCESS: captcha_result=")) {
+						int start=responseText.indexOf("\"")+1;
+						int end=responseText.indexOf("\"", start);
+						captcha=responseText.substring(start,end).toLowerCase();
+					}
+					else if(responseText.startsWith("WAIT")) {
+						Thread.sleep(10000);
+					}
+					else if(responseText.startsWith("FAILURE")) {
+						logger.warning
+						(
+								"Güvenlik kodu çözülürken hata: "+
+								responseText.substring(9)
+						);
+						break;
+					}
+				}
+				(new File(System.getProperty("java.io.tmpdir")+"/captcha")).delete();
+				if (!"".equals(captcha))
+					logger.info("Captcha çözüldü: "+captcha);
+				return captcha;
+			} catch (MalformedURLException e) {
+				logger.warning("Güvenlik kodu çözülürken hata",e);
+			} catch (FileNotFoundException e) {
+				logger.warning("Güvenlik kodu çözülürken hata",e);
+			} catch (IOException e) {
+				logger.warning("Güvenlik kodu çözülürken hata",e);
+			} catch (SAXException e) {
+				logger.warning("Güvenlik kodu çözülürken hata",e);
+			} catch (InterruptedException e) {
+				logger.warning("Güvenlik kodu çözülürken hata",e);
+			}
+			(new File(System.getProperty("java.io.tmpdir")+"/captcha")).delete();
+			return "";
+		}
+		return "";
+	}
+
+	private void copyStream(InputStream in,OutputStream out) throws IOException {
+		byte buf[]=new byte[8*1024];
+		while(true) {
+			int read=in.read(buf);
+			if(read==-1)
+				break;
+			out.write(buf, 0, read);
+		}
+		out.flush();
+	}
+}
---
+++ tags/0.4/src/com/googlecode/jkota/swing/SwingKota.java
@@ -0,0 +1,124 @@
+package com.googlecode.jkota.swing;
+
+import java.awt.AWTException;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+
+import com.googlecode.jkota.BaseKota;
+
+public class SwingKota extends BaseKota implements ActionListener, ClipboardOwner {
+
+	private MenuItem quit,settings,about,logfile,clipboard,statistics;
+	private TrayIcon icon;
+
+	public static void main(String[] args) {
+		if(!SystemTray.isSupported()) {
+			JOptionPane.showMessageDialog(null,"Sistem tepsisi desteklenmiyor.\nJKota kapatılacak", "HATA", JOptionPane.ERROR_MESSAGE);
+			System.exit(1);
+		}
+		
+		SwingUtilities.invokeLater
+		(
+			new Thread() {
+				public void run() {
+					new SwingKota();
+				}
+			}
+		);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==quit)
+			System.exit(0);
+		if(e.getSource()==settings)
+			new SwingSettings();
+		if(e.getSource()==about)
+			new SwingAbout();
+		if(e.getSource()==logfile) {
+			viewLogFile();
+		}
+		if(e.getSource()==clipboard) {
+			StringSelection stringSelection = new StringSelection(icon.getToolTip());
+			Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+			clipboard.setContents( stringSelection, this );
+		}
+		if(e.getSource()==statistics) {
+			new SwingStats();
+		}
+	}
+
+	@Override
+	public void updateQuota(String quota) {
+		icon.setToolTip(quota);
+	}
+
+	@Override
+	public void firstTime() {
+		new SwingSettings();
+	}
+
+	public void lostOwnership(Clipboard clipboard, Transferable contents) {}
+
+	@Override
+	public void initUI() {
+		SystemTray tray =SystemTray.getSystemTray();
+		PopupMenu trayMenu=new PopupMenu();
+		settings=new MenuItem("Ayarlar");
+		settings.addActionListener(this);
+		trayMenu.add(settings);
+		logfile=new MenuItem("Günlük");
+		logfile.addActionListener(this);
+		trayMenu.add(logfile);
+		clipboard=new MenuItem("Panoya kopyala");
+		clipboard.addActionListener(this);
+		trayMenu.add(clipboard);
+		statistics=new MenuItem("İstatistikler");
+		statistics.addActionListener(this);
+		trayMenu.add(statistics);
+		about=new MenuItem("Hakkında");
+		about.addActionListener(this);
+		trayMenu.add(about);
+		quit=new MenuItem("Çıkış");
+		quit.addActionListener(this);
+		trayMenu.add(quit);
+		ClassLoader loader=getClass().getClassLoader();
+		URL fileLocation=loader.getResource("favicon.png");
+		int width=tray.getTrayIconSize().width;
+		int height=tray.getTrayIconSize().height;
+		Image orj = Toolkit.getDefaultToolkit().getImage(fileLocation);
+		icon = new TrayIcon(orj.getScaledInstance(width, height, Image.SCALE_SMOOTH),null,trayMenu);
+		try {
+			tray.add(icon);
+		} catch (AWTException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public String promptForMasterKey() {
+		String masterKey=null;
+		while(masterKey==null)
+			masterKey=JOptionPane.showInputDialog(null, "Ana şifre:");
+		return masterKey;
+	}
+
+	@Override
+	public void showError(Exception e) {
+		SwingUtil.error(null, e,"Ayarlar okunurken hata");
+	}
+}
