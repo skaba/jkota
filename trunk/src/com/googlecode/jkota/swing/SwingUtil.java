eb935030e99c3a33c50ba2dc6ddd5d64dbfa04a9
serkankaba <serkankaba@14afa583-5d3d-0410-b483-632d3007c162>
2008-09-21 08:01:17 +0000

    Kota bilgileri alınmadan istatistik ekranı gösterilemesin.

--- trunk/src/com/googlecode/jkota/swing/SwingUtil.java
+++ trunk/src/com/googlecode/jkota/swing/SwingUtil.java
@@ -8,11 +8,15 @@ import java.io.StringWriter;
 import javax.swing.JOptionPane;
 
 public class SwingUtil {
+	public static void error(Component parent,String message,String title) {
+		JOptionPane.showMessageDialog(parent, message, title, JOptionPane.ERROR_MESSAGE);
+	}
+	
 	public static void error(Component parent,Exception e,String title) {
 		StringWriter out=new StringWriter();
 		e.printStackTrace(new PrintWriter(out));
 		out.flush();
-		JOptionPane.showMessageDialog(parent, out.getBuffer(), title, JOptionPane.ERROR_MESSAGE);
+		error(parent,out.getBuffer().toString(),title);
 	}
 
 	public static void center(Component component) {
