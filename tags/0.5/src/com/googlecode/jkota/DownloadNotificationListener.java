f182b2f01753231bd1921944e49ca3f6213b416f
serkankaba <serkankaba@14afa583-5d3d-0410-b483-632d3007c162>
2008-10-13 18:04:23 +0000

    0.5 etiketi geri alındı.

--- tags/0.5/src/com/googlecode/jkota/DownloadNotificationListener.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.jkota;
-
-import java.util.EventListener;
-
-public interface DownloadNotificationListener extends EventListener {
-	public void notification(DownloadNotificationEvent e);
-}
--- tags/0.5/src/com/googlecode/jkota/DownloadNotificationType.java
+++
@@ -1,5 +0,0 @@
-package com.googlecode.jkota;
-
-public enum DownloadNotificationType {
-	INFO,WARNING;
-}
